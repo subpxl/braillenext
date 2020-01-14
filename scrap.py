368cd9cd2dc2687cc6f47d9ea94e3b58442cdee5
shuhbam314 <shuhbam.r.panchal@gmail.com>
2020-01-14 12:45:58 +0000

    gpio working

--- scrap.py
+++ scrap.py
@@ -5,33 +5,53 @@ from objectdetect import ObjectDetect
 from capture import capture
 import serial
 from gpiozero import Button
+from signal import pause
+
+
+#subprocess.Popen(['sudo','rfcomm','watch','hci0'])
 
 
 ser = serial.Serial('/dev/rfcomm0')
 
-face_button = Button(14)
+face_button = Button(19)
 text_button = Button(15)
 object_button = Button(16)
 location_buton = Button(17)
 emergency_button = Button(18)
 
-
-
-#print(text_within(capture()))
-#print(loacalise_face(capture()))
-
-
 inst = ObjectDetect()
 
+str1 = ("processing please wait").encode()
+str2 = ("its completed").encode()
 
 def face_func():
-    ser.write(loacalise_face(capture()).encode())
+
+    # for testing
+    print(str1)
+    ser.write(str1)
+    str3 = loacalise_face(capture()).encode()
+    ser.write(str3)
+    ser.write(str2)
+    print("The values are %s AND %s" % (str3, str2))
+
 
 def obj_func():
-    ser.write(inst.localize_objects(capture()).encode())
+    print(str1)
+    ser.write(str1)
+    str3 = inst.localize_objects(capture()).encode()
+    ser.write(str3)
+    ser.write(str2)
+    print("The values are %s AND %s" % (str3, str2))
 
 def text_func():
-    ser.write(text_within(capture()).encode())
+    print(str1)
+    ser.write(str1)
+    str3 = text_within(capture()).encode()
+    print("The values are %s AND %s" % (str3, str2))
+
+    
+    ser.write(str3)
+    ser.write(str2)
 
 def location_func():
     ser.write(("locationAsk").encode())
@@ -40,17 +60,13 @@ def location_func():
 def emergency_func():
     ser.write(("emergencyAsk").encode())
 
-    
+# when the button is pressed
 face_button.when_pressed = face_func
 text_button.when_pressed = text_func
 object_button.when_pressed = obj_func
 location_buton.when_pressed = location_func
 emergency_button.when_pressed = emergency_func
 
+pause()
 
 
-#inst = ObjectDetect()
-#result= inst.localize_objects(capture())
-#hh=result.encode()
-#ser.write(hh)
-#print(hh)
