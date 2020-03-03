8ccba955517b11ac51b4255c74491ce64d66f6bb
shubham <shubham.r.panchal@gmail.com>
2020-03-03 23:13:29 +0530

    improved 6

--- scrap.py
+++ scrap.py
@@ -43,3 +43,5 @@ else:
 This greatly decreases the connection delay.
 
  """
+
+# http://www.python-exemplary.com/index_en.php?inhalt_links=navigation_en.inc.php&inhalt_mitte=raspi/en/bluetooth.inc.php
