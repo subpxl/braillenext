b10854d2a47e844b33af74c7d50e901b802dbe27
shubham <shubham.r.panchal@gmail.com>
2020-03-04 21:33:49 +0530

    this it bluetooth pythno test

--- scrap.py
+++ scrap.py
@@ -2,29 +2,30 @@
 
 from btcom import *  # TigerJython
 from btpycom import *  # Standard-Python (PC, Raspi, ...)
+import time
 
 def onStateChanged(state, msg):
     global reply
     if state == "CONNECTING":
-        print "Connecting", msg
+        print ("Connecting", msg)
     elif state == "CONNECTION_FAILED":
-        print "Connection failed", msg
+        print ("Connection failed", msg)
     elif state == "CONNECTED":
-        print "Connected", msg
+        print ("Connected", msg)
     elif state == "DISCONNECTED":
-        print "Disconnected", msg
+        print ("Disconnected", msg)
     elif state == "MESSAGE":
-        print "Message", msg
+        print ("Message", msg)
         reply = msg
        
 serviceName = "EchoServer"
-print "Performing search for service name", serviceName
+print ("Performing search for service name", serviceName)
 client = BTClient(stateChanged = onStateChanged)
 serverInfo = client.findService(serviceName, 20)
 if serverInfo == None:
-    print "Service search failed"
+    print ("Service search failed")
 else:
-    print "Got server info", serverInfo
+    print ("Got server info", serverInfo)
     if client.connect(serverInfo, 20):
         for n in range(0, 101):
             client.sendMessage(str(n))
@@ -37,7 +38,7 @@ else:
 
 
 """
-    serverInfo = ("B8:27:EB:04:A6:7E", 1)
+    serverInfo = ("18:01:F1:06:E0:3A", 1)   // this is for my phone
     connect(serverInfo, 20)
 
 This greatly decreases the connection delay.
