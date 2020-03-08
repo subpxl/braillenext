4177afc6406b924e4e8d9b394147b0bb670ce180
shubham <shubham.r.panchal@gmail.com>
2020-03-08 17:29:17 +0530

    minor 6

--- scrap.py
+++ scrap.py
@@ -1,41 +1,23 @@
-# BtEchoClient.py
+import bluetooth
 
-#from btcom import *  # TigerJython
-from btpycom import *  # Standard-Python (PC, Raspi, ...)
-import time
+bd_addr = "18:01:F1:06:E0:3A"
 
-def onStateChanged(state, msg):
-    global reply
-    if state == "CONNECTING":
-        print ("Connecting", msg)
-    elif state == "CONNECTION_FAILED":
-        print ("Connection failed", msg)
-    elif state == "CONNECTED":
-        print ("Connected", msg)
-    elif state == "DISCONNECTED":
-        print ("Disconnected", msg)
-    elif state == "MESSAGE":
-        print ("Message", msg)
-        reply = msg
-       
-serviceName = "EchoServer"
-print ("Performing search for service name", serviceName)
-client = BTClient(stateChanged = onStateChanged)
-serverInfo = client.findService(serviceName, 20)
-if serverInfo == None:
-    print ("Service search failed")
+port = 1
+
+sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+
+try:
+        sock.connect((bd_addr, port))
+except :
+    print("port1 failed")
 else:
-    print ("Got server info", serverInfo)
-    if client.connect(serverInfo, 20):
-        for n in range(0, 101):
-            client.sendMessage(str(n))
-            reply = ""
-            while reply == "":
-                time.sleep(0.001)
-        client.disconnect()  
+        sock.connect((bd_addr, 20))
+finally:
+    print("cant connect")
 
+sock.send("hello!!")
 
-
+sock.close()
 
 """
     serverInfo = ("18:01:F1:06:E0:3A", 1)   // this is for my phone
@@ -44,5 +26,3 @@ else:
 This greatly decreases the connection delay.
 
  """
-
-# http://www.python-exemplary.com/index_en.php?inhalt_links=navigation_en.inc.php&inhalt_mitte=raspi/en/bluetooth.inc.php
