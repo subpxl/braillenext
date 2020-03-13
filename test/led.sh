345bb1e230b729bbea83573b10bbb69d5be2dd64
shubham <shubham.r.panchal@gmail.com>
2020-03-13 19:23:09 +0530

    minor

--- test/led.sh
+++ test/led.sh
@@ -4,4 +4,4 @@ echo 17 > /sys/class/gpio/export
 #Lo configuramos como salida 
 echo out > /sys/class/gpio/gpio17/direction 
 #Encendemos el LED asignandole 1 como valor lógico
-echo 1 > /sys/class/gpio/gpio17/value
+echo "1" > /sys/class/gpio/gpio17/value
